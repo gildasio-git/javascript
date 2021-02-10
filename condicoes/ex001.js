c33cbff6657082ebe58bd82d59a061b65cb7d581
Gildasio <gildasio.ariquemes@hotmail.com>
2021-02-09 22:00:03 -0400

    Exercicio 01 praticado

---
+++ condicoes/ex001.js
@@ -0,0 +1,8 @@
+/* INSTRUÇÃO SIMPLES  */
+var vel = 12
+console.log(`A velocidade do seu crro é ${vel}km/h`)
+
+if(vel >=60){
+    console.log(`Voce ultrapassou o limite de velocidade, MULTADO!`)
+}
+console.log(`Diriga com cuidade e sempr use o sinte de segurança`)
---
+++ condicoes/ex002.js
@@ -0,0 +1,10 @@
+/* INSTRUÇÃO COMPOSTA */
+
+console.log(`vivendo em ${pais}`)
+var pais = 'Br'
+
+if(pais == 'Br'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
