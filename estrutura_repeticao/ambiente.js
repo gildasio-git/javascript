84c5a5f48e04e676c01c2fb92e01750f35581803
Gildasio <gildasio.ariquemes@hotmail.com>
2021-02-17 12:08:55 -0400

    Esturuta repetiçao - exercicios praticados

---
+++ estrutura_repeticao/ambiente.js
@@ -0,0 +1,29 @@
+//WHILE (ENQUANTO) | Esturtura de repetição com teste lógico no início
+
+/*
+var c = 1 
+
+while(c <= 100){
+    console.log(`Passo ${c}`)
+    c++ //Inclemento
+}
+
+*/
+
+
+/*
+//DO (FAÇA) | Estrutura de repetição com teste lógico no final 
+var c = 1
+
+do{
+ console.log(`Passo ${c}`)
+ c++
+}while(1<=6)
+*/
+
+//ESTRURUTA DE REPETIÇÃO COM VARIÁVEL DE CONTROLE | USADO QUANDO VOCẼ SABE O LIMITE DAS SUAS EXECUÇÕES
+for (c = 1; c < 10; c++) {
+    console.log(`passo ${c}`)
+    
+}
+console.log('FIM')
---
+++ estrutura_repeticao/exercicio01/script.js
@@ -0,0 +1,21 @@
+function contar(){
+    
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var stop = document.getElementById('passo')
+    var res = document.getElementById('res')
+
+    if(Number(ini.value == 0) || (Number(fim.value == 0) || (Number(stop.value == 0)))) {
+        alert('Valores das variaveis vazil, preencha e tente novamente')
+        res.innerHTML = `Variáveis com valores vazios, preencha e tente novamente`
+    }else if(Number(passo.value > Number(fim.value) )){
+        res.innerHTML = `Campo passo não pode ser maior que inicio`
+        res.style.color = 'red'        
+        }
+
+
+        //res.innerHTML =`Valore alocados nas varíaveis ${ini.value}, ${fim.value}, ${stop.value}`
+    
+}
+
+
