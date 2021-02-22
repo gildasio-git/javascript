c9a64cbf372661be0ac9a98021a0dcaf817c91bf
Gildasio <gildasio.ariquemes@hotmail.com>
2021-02-22 00:34:09 -0400

    Resoluçao exercicio contar.js

--- estrutura_repeticao/exercicio01/script.js
+++ estrutura_repeticao/exercicio01/script.js
@@ -1,29 +1,51 @@
 function contar(){
- 
-    var ini = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var stop = document.getElementById('passo')
+    
+    var ini = document.getElementById('txtini')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('passo')
     var res = document.getElementById('res')
 
-    /** 
-    if(Number(ini.value == 0) || (Number(fim.value == 0) || (Number(stop.value == 0)))) {
-        alert('Valores das variaveis vazil, preencha e tente novamente')
-        res.innerHTML = `Variáveis com valores vazios, preencha e tente novamente`
-    }else if(Number(passo.value > Number(fim.value) )){
-        res.innerHTML = `Campo passo não pode ser maior que inicio`
-        res.style.color = 'red'        
+     
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('faltam dados, preencha e tente novamente')
+        res.innerHTML = `Impossível contar`
+
+    }else{
+        res.innerHTML = `Contando... `
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)    
+        
+        //Testa se o passo é igual a 0
+        if(p <=0 ){
+            window.alert('passo inválido, considerando passo 1')
+            p = 1
+
+        }
+
+        //Testa os valores 
+        //Contatem crescente
+        if(i < f){
+            for(var c=i;c<=f;c+=p){
+               
+                //Concatenando os valores
+                //Para inclusão dos emogis accesse o unicod dos mesmos pelo site :
+                //https://unicode.org/emoji/charts/full-emoji-list.html
+                res.innerHTML += 	` ${c} \u{1F449}`
+              
+              }
+               
+
+        //Contagem decrescente
+        }else {
+            for(var c=i;c >= f;c -= p){
+                res.innerHTML += `${c} \u{1f449}`
+            }
+            
+        }
+        res.innerHTML += `\u{1F3c1}`
+    
         }
        
-        //res.innerHTML =`Valore alocados nas varíaveis ${ini.value}, ${fim.value}, ${stop.value}`
-        */
-  
-   for(ini=ini;ini<fim;ini++){
-            
-            ini++
-   }
-   console.log(ini.value)
-         
-        
-    }
-   
 
+}
